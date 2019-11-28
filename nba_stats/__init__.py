f76cb5193df531ae595618c837a36fddfd8c2dd7
Jacob Pavlock <jtpavlock@gmail.com>
2019-11-27 19:48:02 -0800

    cache api requests for 1 week instead of 1 day

--- nba_stats/__init__.py
+++ nba_stats/__init__.py
@@ -1,4 +1,4 @@
 import requests_cache
 
-# cache api responses for 1 day
-requests_cache.install_cache(expire_after=86400)
+# cache api responses for 1 week
+requests_cache.install_cache(expire_after=604800)
