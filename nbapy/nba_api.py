26b1a6b96ade2cb554dcd2e63b10e26845e00e33
Jacob Pavlock <jtpavlock@gmail.com>
2020-07-18 19:10:00 -0700

    fix multiple accept headers causing failures

--- nbapy/nba_api.py
+++ nbapy/nba_api.py
@@ -17,7 +17,7 @@ class NbaAPI:
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0)"
             " Gecko/20100101 Firefox/61.0"
         ),
-        "Accept": "application/json, text/plain, */*",
+        "Accept": "application/json",
         "Accept-Language": "en-US,en;q=0.5",
         "Referer": "https://stats.nba.com/",
         "Accept-Encoding": "gzip, deflate, br",
